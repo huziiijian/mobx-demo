4ca120aa93dfee6360d2baa3f63d66e89dc329c9
huzijian <772964145@qq.com>
2020-06-15 22:20:32 +0800

    delete by incaution

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import store from './store/index'
 import { Provider } from 'mobx-react'
+import Home from './pages/home'
 
 const App = () => {
   console.log("work")
   return (
     <div className="App">
-      <Provider store={store}></Provider>
+      <Provider store={store}>
+      <Home/>
+      </Provider>
     </div>
   );
 }
---
+++ src/pages/home/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div className='home'>
+      <h1>This is home!</h1>
+      </div>
+    )
+  }
+}
