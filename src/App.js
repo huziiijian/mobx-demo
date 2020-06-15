188182664f7e7cc5df6519cbf188271eb3eefc18
huzijian <772964145@qq.com>
2020-06-15 21:57:45 +0800

    decorators-legacy-problem

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import store from './store/index'
 import { Provider } from 'mobx-react'
 
 const App = () => {
+  console.log("work")
   return (
     <div className="App">
       <Provider store={store}></Provider>
