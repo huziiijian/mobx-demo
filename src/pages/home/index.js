693ebdaad0090945a6a31028b20b4a6e3f2164f3
huziiijian <772964145@qq.com>
2020-07-13 09:26:47 +0800

    d

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,13 +3,12 @@ import React, { Component } from 'react'
 export default class Home extends Component {
 
   state = {
-
   }
 
   render() {
     return (
       <div className='home'>
-      <h1>This is home!</h1>
+        <h1>This is home!</h1>
       </div>
     )
   }
